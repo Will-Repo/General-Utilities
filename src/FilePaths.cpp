3e54a086d09a962e34e02d40f7e7a56ec61cd287
Will <wricks2023.16@gmail.com>
2026-04-12 21:32:03 +0100

    Updated filepaths to be simple struct and get method for executable path

--- src/FilePaths.cpp
+++ src/FilePaths.cpp
@@ -4,15 +4,15 @@
 #include <iostream>
 #include <filesystem>
 
-std::string setExecutablePath() {
+std::string getExecutablePath() {
     int dirname_length;
     int length = wai_getExecutablePath(NULL, 0, NULL);
     auto path = (char*)malloc(length + 1);
     wai_getExecutablePath(path, length, &dirname_length);
     path[length] = '\0';
     std::filesystem::path fPath = path;
-    std::cout << "Executable path: " << fPath << std::endl;
+    //std::cout << "Executable path: " << fPath << std::endl;
     fPath = fPath.parent_path();
-    std::cout << "Executable folder: " << fPath << std::endl;
+    //std::cout << "Executable folder: " << fPath << std::endl;
     return fPath;
 }
--- src/FilePaths.h
+++ src/FilePaths.h
@@ -1,15 +1,13 @@
 #include <string>
 #pragma once
-std::string setExecutablePath();
+std::string getExecutablePath();
 
 //Paths are relative to the executable path.
-class FilePaths {
-    public:
-        //Paths are from the executable path. By defualt, this assumes the executable is in the build/app directory.
-        std::string srcPath = "../../app/src";
-        std::string shadersPath = "../../app/shaders";
-        std::string fontsPath = "../../app/fonts";
-        std::string assetsPath = "../../app/assets";
-        std::string corePath = "../../core";
-        const std::string executablePath = setExecutablePath();
+struct FilePaths {
+    //Paths are from the executable path. By defualt, this assumes the executable is in the build/app directory.
+    std::string srcPath = "../../app/src";
+    std::string shadersPath = "../../app/shaders";
+    std::string fontsPath = "../../app/fonts";
+    std::string assetsPath = "../../app/assets";
+    std::string corePath = "../../core";
 };
